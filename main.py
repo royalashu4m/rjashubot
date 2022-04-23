71f10545bc1dd79d9f0f48786a718a434fe0bc3d
ashish kumar <84221198+royalashu4m@users.noreply.github.com>
2022-04-23 09:04:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ async def start_bot():
     await idle()
     print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
-    await bot.join_chat("TrickyAbhii_Op")
+    await bot.join_chat("LittelStar_org")
     await Herox.send_message(
                SUPPORT,
             "<b>Congrats!! Music Bot has started successfully!</b>",
